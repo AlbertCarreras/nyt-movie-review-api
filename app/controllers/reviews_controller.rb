edb79b031ed7b9859ec787f5f00a8459feae17c4
Alberto Carreras <a.carreras.c@gmail.com>
2018-07-26 23:28:56 -0400

    add .env and hide api key

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,14 +4,14 @@ require 'json'
 class ReviewsController < ApplicationController
 
     def show_last_reviews
-        @reviews = RestClient.get("https://api.nytimes.com/svc/movies/v2/reviews/all.json?api-key=#{ENV['API_KEY']}")
+        @reviews = RestClient.get("https://api.nytimes.com/svc/movies/v2/reviews/all.json?api-key=#{process.env.API_KEY}")
         @reviews = JSON.parse(@reviews)
         
         render json: @reviews
     end
 
     def show_search_reviews
-        @reviews = RestClient.get("https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=#{ENV['API_KEY']}&query=#{params["search"]}")
+        @reviews = RestClient.get("https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=#{process.env.API_KEY}&query=#{params["search"]}")
         @reviews = JSON.parse(@reviews)
         
         render json: @reviews
