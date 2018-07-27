3055982c50f48dd727cf13fdcd39d550e49e6183
Alberto Carreras <a.carreras.c@gmail.com>
2018-07-26 22:38:37 -0400

    add both routes. work

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,8 +3,16 @@ require 'json'
 
 class ReviewsController < ApplicationController
 
-    def show
+    def show_last_reviews
         @reviews = RestClient.get("https://api.nytimes.com/svc/movies/v2/reviews/all.json?api-key=c4c50c9ebc8f4db5a0c071c9de2c6d68")
+        @reviews = JSON.parse(@reviews)
+        
+        render json: @reviews
+    end
+
+    def show_search_reviews
+        byebug
+        @reviews = RestClient.get("https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=c4c50c9ebc8f4db5a0c071c9de2c6d68&query=#{params["search"]}")
         @reviews = JSON.parse(@reviews)
         
         render json: @reviews
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/lastreviews" => "reviews#show"
-  get "/searchreviews" => "reviews#show"
+  get "/lastreviews" => "reviews#show_last_reviews"
+  get "/searchreviews" => "reviews#show_search_reviews"
 
 
 end
