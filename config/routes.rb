bd340a73e22621c343c07cc759caddb9e8e640f9
Alberto Carreras <a.carreras.c@gmail.com>
2018-07-26 22:29:06 -0400

    modify route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/reviews" => "reviews#show"
+  get "/lastreviews" => "reviews#show"
+  get "/searchreviews" => "reviews#show"
+
 
 end
