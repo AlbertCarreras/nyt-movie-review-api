e29f3074fdec3a5a4ae51dd4a8788936ec0f4fb4
Alberto Carreras <a.carreras.c@gmail.com>
2018-07-26 22:44:30 -0400

    add both routes. work

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -11,7 +11,6 @@ class ReviewsController < ApplicationController
     end
 
     def show_search_reviews
-        byebug
         @reviews = RestClient.get("https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=c4c50c9ebc8f4db5a0c071c9de2c6d68&query=#{params["search"]}")
         @reviews = JSON.parse(@reviews)
         
